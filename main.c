f7cbd15fe0036739a5f15ae05b36918c1ec66298
kgndnc <hkagandnc@gmail.com>
2022-01-09 01:53:12 +0300

    insertNode() function added

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 struct treeNode  {
     int data;
     struct treeNode* leftPtr;
@@ -11,7 +13,45 @@ void preorder(TreeNodePtr treePtr);
 void inorder(TreeNodePtr treePtr);
 void postorder(TreeNodePtr treePtr);
 
+TreeNodePtr newNode(int value) {
+    TreeNodePtr treePtr = (TreeNodePtr) malloc(sizeof(TreeNode));
+    treePtr->data = value;
+    treePtr->leftPtr = NULL;
+    treePtr->rightPtr = NULL;
+    return treePtr;
+}
+void insertNode(TreeNodePtr* treePtr, int value){
+    // If the node is empty assign the value to its data variable
+    if( *treePtr == NULL) {
+        *treePtr = newNode(value);
+        return;
+    }
+    // If the value to be inserted is less than the current node's data than move on to the left of tree.
+    else if (value < (*treePtr)->data) {
+        // If the left child node of this node is empty it means that we found
+        // value's place. We establish a link between parent and child nodes.
+        if((*treePtr)->leftPtr == NULL) {
+            (*treePtr)->leftPtr = newNode(value);
+            return;
+        }
+        insertNode ( &(*treePtr)->leftPtr, value);
+    }// If the value to be inserted is greater than the current node's data than move on to the left of tree.
+    else if (value > (*treePtr)->data) {
+        // If the right child node of this node is empty it means that we found
+        // value's place. We establish a link between parent and child nodes.
+        if((*treePtr)->rightPtr == NULL) {
+            (*treePtr)->rightPtr = newNode(value);
+            return;
+        }
+        insertNode ( &(*treePtr)->rightPtr, value);
+    }
+    // If the value is the as the current node's data then don't insert it.
+}
 int main() {
-    printf("Hello, World!\n");
+    TreeNodePtr root;
+
+
+
+
     return 0;
 }
